16130e6ca5a50a1f9104172ed83471320b37f0f2
Sairam Sriram <67696992+ssriram22@users.noreply.github.com>
2024-04-13 03:53:20 +0000

    adding latest code

---
+++ listPrac1.py
@@ -0,0 +1,5 @@
+numbersList=["one", "two", "three", "four"]
+for i in numbersList:
+    print(i)
+    print(type(numbersList))
+
---
+++ loops/for1.py
@@ -0,0 +1,12 @@
+#program 1
+
+# for i in range(10):
+#     print("you are on track")
+
+
+#program 2
+
+basket=("fruits", "vegetables", "groceries", "stationary", "kitchen-set")
+for i in basket:
+    print(i)
+
