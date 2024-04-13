bc08504bfa92ab4c2ae7ddcd43c584173bd9a9c6
Sairam Sriram <67696992+ssriram22@users.noreply.github.com>
2024-04-13 04:36:34 +0000

    latest code

--- loops/for1.py
+++ loops/for1.py
@@ -1,12 +1,16 @@
 #program 1
 
-# for i in range(10):
-#     print("you are on track")
+for i in range(10):
+    print("you are on track")
 
 
 #program 2
 
-basket=("fruits", "vegetables", "groceries", "stationary", "kitchen-set")
-for i in basket:
-    print(i)
+# basket=("fruits", "vegetables", "groceries", "stationary", "kitchen-set")
+# for i in basket:
+#     print(i)
 
+#program 3
+
+# for i in range(1,6):
+#     print("you are on track")
---
+++ loops/while1.py
@@ -0,0 +1,4 @@
+i=5
+while i <= 10:
+    print("Good while")
+    i=i+1
